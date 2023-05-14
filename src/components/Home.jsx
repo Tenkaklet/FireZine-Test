dbabb59b70b1886c29c884aaa4f393a91dbd3bb0
Max Carlquist <tenkaklet@gmail.com>
2023-05-14 12:03:01 +0200

    Adding image

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Button, Container, Image } from 'react-bootstrap';
+import friends from '../assets/us.jpg';
 
 const Home = () => {
   return (
@@ -7,7 +8,7 @@ const Home = () => {
       <Container>
         <header className='mt-3 hero'>
           <h1 className='text-center'>Login or Sign up to Zine Me Up!</h1>
-          <Image src='src/assets/us.jpg' thumbnail />
+          <Image src={friends} thumbnail />
         </header>
         <main className='home-main' role='main'>
           <Button href='/registration'>Go to Register!</Button>
